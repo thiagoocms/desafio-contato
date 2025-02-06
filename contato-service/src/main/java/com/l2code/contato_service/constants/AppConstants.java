3d54afc827e47b4f928a7c2eff96f3d3f695d758
thiagoocms <thiagocms13@gmail.com>
2025-02-05 21:21:37 -0300

    criando o controller

---
+++ contato-service/src/main/java/com/l2code/contato_service/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.l2code.contato_service.constants;
+
+public final class AppConstants {
+
+    public static final String PATH = "/contato";
+    public static final String API = "/api";
+    public static final String V1 = "/v1";
+
+    private AppConstants() {
+    }
+
+}
---
+++ contato-service/src/main/java/com/l2code/contato_service/rest/ContatoResource.java
@@ -0,0 +1,80 @@
+package com.l2code.contato_service.rest;
+
+import com.l2code.contato_service.constants.AppConstants;
+import com.l2code.contato_service.dto.contato.ContatoDTO;
+import com.l2code.contato_service.dto.contato.ListContatoDTO;
+import com.l2code.contato_service.dto.contato.SimpleContatoDTO;
+import com.l2code.contato_service.mapper.ContatoMapper;
+import com.l2code.contato_service.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping(value = AppConstants.PATH + AppConstants.API + AppConstants.V1 + "/contatos")
+public class ContatoResource {
+
+    private final ContatoService contatoService;
+
+    @Autowired
+    public ContatoResource(ContatoService contatoService) {
+        this.contatoService = contatoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContatoDTO> create(@RequestBody SimpleContatoDTO dto, UriComponentsBuilder uriBuilder) {
+
+        var entity = ContatoMapper.toEntity(dto);
+
+        this.contatoService.create(entity);
+
+        var contatoDTO = ContatoMapper.toDto(entity);
+
+        var uri = uriBuilder.path("/contatos/{id}").buildAndExpand(entity.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(contatoDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ContatoDTO> update(@PathVariable Long id, @RequestBody SimpleContatoDTO dto) {
+
+        var entity = ContatoMapper.toEntity(dto);
+
+        entity = this.contatoService.update(id, entity);
+
+        var contatoDTO = ContatoMapper.toDto(entity);
+
+        return ResponseEntity.ok(contatoDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<ListContatoDTO> findAll() {
+        var response = this.contatoService.findAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}/favoritar")
+    public ResponseEntity<Void> favorite(@PathVariable Long id) {
+
+        this.contatoService.favorite(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/desfavoritar")
+    public ResponseEntity<Void> unFavorite(@PathVariable Long id) {
+
+        this.contatoService.unFavorite(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/desativar")
+    public ResponseEntity<Void> inactivate(@PathVariable Long id) {
+
+        this.contatoService.inactivate(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
